7d3e10db00e936e94a9089052aa8a8507317d42e
mmgoddard <mmgoddard@dundee.ac.uk>
2014-09-16 21:38:58 +0100

    Change to Fragment/TabHost from TabActivity

---
+++ Industrial Project/PotatoIdentifier/src/com/app/potatoidentifer/activities/BaseFragment.java
@@ -0,0 +1,14 @@
+package com.app.potatoidentifer.activities;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Mark on 16/09/2014.
+ */
+public class BaseFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- Industrial Project/PotatoIdentifier/src/com/app/potatoidentifer/activities/MainActivity.java
+++ Industrial Project/PotatoIdentifier/src/com/app/potatoidentifer/activities/MainActivity.java
@@ -1,45 +1,29 @@
 package com.app.potatoidentifer.activities;
 
 import android.os.Bundle;
-import android.app.TabActivity;
-import android.content.Intent;
-import android.widget.TabHost;
-import android.widget.TabHost.TabSpec;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
 import com.example.potatoidentifier.R;
 
-// The way i have done the tabs is deprecated so perhaps there is a better way of doing them
-@SuppressWarnings("deprecation")
-public class MainActivity extends TabActivity {
+public class MainActivity extends FragmentActivity {
+    private FragmentTabHost mTabHost;
 
-    /**
-     * Called when the activity is first created.
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // create the TabHost that will contain the Tabs
-        TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
+        mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
 
-        TabSpec tab1 = tabHost.newTabSpec("First Tab");
-        TabSpec tab2 = tabHost.newTabSpec("Second Tab");
-        TabSpec tab3 = tabHost.newTabSpec("Third tab");
-
-        // Set the Tab name and Activity
-        // that will be opened when particular Tab will be selected
-        tab1.setIndicator("Glossary");
-        tab1.setContent(new Intent(this, GlossaryTab.class));
-
-        tab2.setIndicator("Questions");
-        tab2.setContent(new Intent(this, QuestionTab.class));
-
-        tab3.setIndicator("Video");
-        tab3.setContent(new Intent(this, VideoTab.class));
-
-        /** Add the tabs  to the TabHost to display. */
-        tabHost.addTab(tab1);
-        tabHost.addTab(tab2);
-        tabHost.addTab(tab3);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("glossaryTab").setIndicator("Glossary", null),
+                GlossaryFragment.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("questionTab").setIndicator("Question", null),
+                QuestionFragment.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("videoTab").setIndicator("Video", null),
+                VideoFragment.class, null);
     }
 } 
