eb7f2f274e421c4efa6c35a711d96cfe149770c1
mmgoddard <mmgoddard@dundee.ac.uk>
2014-09-16 17:47:13 +0100

    Tab sit at bottom of screen.

--- Industrial Project/PotatoIdentifier/src/com/example/tabexample/GlossaryTab.java
+++ Industrial Project/PotatoIdentifier/src/com/example/tabexample/GlossaryTab.java
@@ -2,19 +2,13 @@ package com.example.tabexample;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-public class GlossaryTab extends Activity
-{
+public class GlossaryTab extends Activity {
         @Override
-        public void onCreate(Bundle savedInstanceState)
-        {
+        public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             
             // Placeholder. But calls this layout to display the page
             setContentView(R.layout.tab_layout_1);
-            
         }
 }
--- Industrial Project/PotatoIdentifier/src/com/example/tabexample/VideoTab.java
+++ Industrial Project/PotatoIdentifier/src/com/example/tabexample/VideoTab.java
@@ -5,20 +5,19 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.widget.TextView;
 
-public class VideoTab  extends Activity
-{
-        @Override
-        public void onCreate(Bundle savedInstanceState)
-        {
-        	// Different to gloassary, this calls text dynamically. But i think the glossary page is the
-        	// proper way of doing things
-            super.onCreate(savedInstanceState);
-            
-            TextView  tv=new TextView(this);
-            tv.setTextSize(25);
-            tv.setGravity(Gravity.CENTER_VERTICAL);
-            tv.setText("This Is Tab3 Activity");
-            
-            setContentView(tv);
-        }
+public class VideoTab extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // Different to glossary, this calls text dynamically. But i think the glossary page is the
+        // proper way of doing things
+        super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+        tv.setTextSize(25);
+        tv.setGravity(Gravity.CENTER_VERTICAL);
+        tv.setText("This Is Tab3 Activity");
+
+        setContentView(tv);
+    }
 }
