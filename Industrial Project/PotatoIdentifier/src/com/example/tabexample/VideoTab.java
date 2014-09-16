35961a8ae9239047f751be1ad4f6f0834a880542
RobertJamesMason <rjmason93@gmail.com>
2014-09-16 14:49:16 +0100

    Uploading of first tab files
    Quick code tab thing for us to use.

---
+++ Industrial Project/PotatoIdentifier/src/com/example/tabexample/VideoTab.java
@@ -0,0 +1,24 @@
+package com.example.tabexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
+
+public class VideoTab  extends Activity
+{
+        @Override
+        public void onCreate(Bundle savedInstanceState)
+        {
+        	// Different to gloassary, this calls text dynamically. But i think the glossary page is the
+        	// proper way of doing things
+            super.onCreate(savedInstanceState);
+            
+            TextView  tv=new TextView(this);
+            tv.setTextSize(25);
+            tv.setGravity(Gravity.CENTER_VERTICAL);
+            tv.setText("This Is Tab3 Activity");
+            
+            setContentView(tv);
+        }
+}
