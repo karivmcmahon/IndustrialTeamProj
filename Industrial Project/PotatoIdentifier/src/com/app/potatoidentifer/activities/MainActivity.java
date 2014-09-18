095b280953b7042620f03eee29b2b5cbdfd1fd61
mmgoddard <mmgoddard@dundee.ac.uk>
2014-09-18 13:13:05 +0100

    Changed tabs to icons

--- Industrial Project/PotatoIdentifier/src/com/app/potatoidentifer/activities/MainActivity.java
+++ Industrial Project/PotatoIdentifier/src/com/app/potatoidentifer/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.app.potatoidentifer.activities;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
@@ -13,17 +14,18 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Resources res = getResources();
         mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
         mTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
 
         mTabHost.addTab(
-                mTabHost.newTabSpec("glossaryTab").setIndicator("Glossary", null),
+                mTabHost.newTabSpec("glossaryTab").setIndicator("", res.getDrawable(R.drawable.ic_tab_glossary)),
                 GlossaryFragment.class, null);
         mTabHost.addTab(
-                mTabHost.newTabSpec("questionTab").setIndicator("Question", null),
+                mTabHost.newTabSpec("questionTab").setIndicator("", res.getDrawable(R.drawable.ic_tab_glossary)),
                 QuestionFragment.class, null);
         mTabHost.addTab(
-                mTabHost.newTabSpec("videoTab").setIndicator("Video", null),
+                mTabHost.newTabSpec("videoTab").setIndicator("", res.getDrawable(R.drawable.ic_tab_glossary)),
                 VideoFragment.class, null);
     }
 } 
