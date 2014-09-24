fd4b4535e0c4047d6d5eb608e6b118b0f8e84604
mmgoddard <mmgoddard@dundee.ac.uk>
2014-09-24 15:49:37 +0100

    Fixed scroll issue with video.

--- Industrial Project/PotatoIdentifier/src/com/app/potatoidentifer/activities/VideoFragment.java
+++ Industrial Project/PotatoIdentifier/src/com/app/potatoidentifer/activities/VideoFragment.java
@@ -8,95 +8,96 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.ImageButton;
 import android.widget.MediaController;
 import android.widget.ScrollView;
 import android.widget.VideoView;
-
 import com.example.potatoidentifier.R;
 
 /**
  * This class creates a video fragment to display the video tutorials
- * @author Kari
  *
+ * @author Kari
  */
 public class VideoFragment extends BaseFragment {
-	View v;
-	MediaController mediaControls;
-	MediaController mediaControls2;
-	
+    View v;
+    MediaController mediaControls;
+    MediaController mediaControls2;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.video_fragment_layout, container, false);
-       
+
         ScrollView sv = (ScrollView) v.findViewById(R.id.scrollview);
         sv.smoothScrollTo(0, 0);
-        splashPlayer();
+        splashPlayer(sv);
         return v;
     }
-    
-    public void splashPlayer() 
-    {
-    	 final VideoView videoHolderTest1 = (VideoView) v.findViewById(R.id.videoView1);
-    	 final ImageButton playButton = (ImageButton) v.findViewById(R.id.play_button);
-    	 final VideoView videoHolderTest2 = (VideoView) v.findViewById(R.id.videoView2);
-    	 final ImageButton playButton2 = (ImageButton) v.findViewById(R.id.play_button2);
-    	 Uri video1 = Uri.parse("android.resource://" + getActivity().getPackageName() + "/"
-    	    + R.raw.lfd2);
-    	 Uri video2 = Uri.parse("android.resource://" + getActivity().getPackageName() + "/"
-    	    	    + R.raw.fta);
-    	 if (mediaControls == null) 
-    	 {
-    	      mediaControls = new MediaController(getActivity());
-    	 }
-    	 if (mediaControls2 == null)
-    	 {
-    		 mediaControls2 = new MediaController(getActivity());
-    	 }
-    	 mediaControls.setAnchorView(videoHolderTest1);
-    	 videoHolderTest1.setMediaController(mediaControls);
-    	 videoHolderTest1.setVideoURI(video1);
-    	 videoHolderTest1.setFocusable(false);
-    	 mediaControls2.setAnchorView(videoHolderTest2);
-    	 videoHolderTest2.setMediaController(mediaControls2);
-    	 videoHolderTest2.setVideoURI(video2);
-    	 videoHolderTest2.setFocusable(false);
-    	 playButton.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				videoHolderTest1.start();
-				mediaControls.show();
-				playButton.setVisibility(View.GONE);
-			}
-    		 
-    	 });
-    	
-    	 videoHolderTest1.setOnCompletionListener(new OnCompletionListener() {
-    	  public void onCompletion(MediaPlayer mp) {
-    	    playButton.setVisibility(View.VISIBLE);
-    	  }
-    	 });
-    	 
-    	 
-    	 playButton2.setOnClickListener(new OnClickListener() {
+    public void splashPlayer(ScrollView sv) {
+        final VideoView videoHolderTest1 = (VideoView) v.findViewById(R.id.videoView1);
+        final ImageButton playButton = (ImageButton) v.findViewById(R.id.play_button);
+        final VideoView videoHolderTest2 = (VideoView) v.findViewById(R.id.videoView2);
+        final ImageButton playButton2 = (ImageButton) v.findViewById(R.id.play_button2);
+        Uri video1 = Uri.parse("android.resource://" + getActivity().getPackageName() + "/"
+                + R.raw.lfd2);
+        Uri video2 = Uri.parse("android.resource://" + getActivity().getPackageName() + "/"
+                + R.raw.fta);
+        if (mediaControls == null) {
+            mediaControls = new MediaController(getActivity());
+        }
+        if (mediaControls2 == null) {
+            mediaControls2 = new MediaController(getActivity());
+        }
+        mediaControls.setAnchorView(videoHolderTest1);
+        videoHolderTest1.setMediaController(mediaControls);
+        videoHolderTest1.setVideoURI(video1);
+        videoHolderTest1.setFocusable(false);
+        mediaControls2.setAnchorView(videoHolderTest2);
+        videoHolderTest2.setMediaController(mediaControls2);
+        videoHolderTest2.setVideoURI(video2);
+        videoHolderTest2.setFocusable(false);
 
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				videoHolderTest2.start();
-				mediaControls2.show();
-				playButton2.setVisibility(View.GONE);
-			}
-    		 
-    	 });
-    	
-    	 videoHolderTest2.setOnCompletionListener(new OnCompletionListener() {
-    	  public void onCompletion(MediaPlayer mp) {
-    	    playButton2.setVisibility(View.VISIBLE);
-    	  }
-    	 });
-       
-    	}
+        playButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                videoHolderTest1.start();
+                mediaControls.show();
+                playButton.setVisibility(View.GONE);
+            }
+
+        });
+
+        videoHolderTest1.setOnCompletionListener(new OnCompletionListener() {
+            public void onCompletion(MediaPlayer mp) {
+                playButton.setVisibility(View.VISIBLE);
+            }
+        });
+
+        playButton2.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                videoHolderTest2.start();
+                mediaControls2.show();
+                playButton2.setVisibility(View.GONE);
+            }
+
+        });
+
+        videoHolderTest2.setOnCompletionListener(new OnCompletionListener() {
+            public void onCompletion(MediaPlayer mp) {
+                playButton2.setVisibility(View.VISIBLE);
+            }
+        });
+
+        //If screen is scrolled, hide the media controls.
+        sv.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+            @Override
+            public void onScrollChanged() {
+                mediaControls2.hide();
+                mediaControls.hide();
+            }
+        });
+    }
 }
