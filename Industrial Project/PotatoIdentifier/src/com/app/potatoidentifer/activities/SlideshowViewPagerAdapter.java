571266444e33a95460110067d3794551d4823bb2
karivmcmahon <kmc_xo@hotmail.co.uk>
2014-09-24 14:21:25 +0100

    Test

--- Industrial Project/PotatoIdentifier/src/com/app/potatoidentifer/activities/SlideshowViewPagerAdapter.java
+++ Industrial Project/PotatoIdentifier/src/com/app/potatoidentifer/activities/SlideshowViewPagerAdapter.java
@@ -48,7 +48,7 @@ public class SlideshowViewPagerAdapter extends PagerAdapter {
 	}
 
 	public Object instantiateItem(ViewGroup collection, final int position) {
-		
+		final BitmapScaler scaler;
 		ImageView view = new ImageView(activity);
 		view.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
 				LayoutParams.MATCH_PARENT));
@@ -57,48 +57,51 @@ public class SlideshowViewPagerAdapter extends PagerAdapter {
 
         Resources res = context.getResources();
         try {
-            BitmapScaler scaler = new BitmapScaler(res, imageArray.get(position), 750);
+            scaler = new BitmapScaler(res, imageArray.get(position), 750);
             view.setImageBitmap(scaler.getScaled());
         } catch(IOException e) {
             e.printStackTrace();
         }
+       
 		collection.addView(view, 0);
-//		view.setOnClickListener(new OnClickListener() {
-//
-//			@SuppressLint("NewApi")
-//			@Override
-//			public void onClick(View arg0) {
-//				DisplayMetrics dm = new DisplayMetrics();
-//				activity.getWindowManager().getDefaultDisplay().getMetrics(dm);
-//				int width = dm.widthPixels;
-//				int height = dm.heightPixels;
-//
-//				Bitmap bitmap = BitmapFactory.decodeResource(
-//						activity.getResources(), imageArray.get(position));
-//				// Get target image size
-//				int bitmapHeight = bitmap.getHeight();
-//				int bitmapWidth = bitmap.getWidth();
-//
-//				BitmapDrawable resizedBitmap = new BitmapDrawable(activity
-//						.getResources(), Bitmap.createScaledBitmap(bitmap,
-//						width, width * bitmapHeight / bitmapWidth, false));
-//
-//				final Dialog dialog = new Dialog(activity);
-//				dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-//				dialog.setContentView(R.layout.full_screen_image);
-//				dialog.setCancelable(true);
-//
-//				// Regular image view
-//				// ImageView img = (ImageView)
-//				// dialog.findViewById(R.id.imageView1);
-//				// img.setBackground(resizedBitmap);
-//
-//				// TODO set up image view with pinch for zoom - Needs tested on
-//				// device not sure it works
-//				TouchImageView img = (TouchImageView) dialog
-//						.findViewById(R.id.imageView1);
-//				//img.setBackground(resizedBitmap);
-//
+		view.setOnClickListener(new OnClickListener() {
+
+			@SuppressLint("NewApi")
+			@Override
+			public void onClick(View arg0) {
+				DisplayMetrics dm = new DisplayMetrics();
+				activity.getWindowManager().getDefaultDisplay().getMetrics(dm);
+				int width = dm.widthPixels;
+				int height = dm.heightPixels;
+
+				
+
+				final Dialog dialog = new Dialog(activity);
+				dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+				dialog.setContentView(R.layout.full_screen_image);
+				dialog.setCancelable(true);
+				
+				Bitmap bitmap = BitmapFactory.decodeResource(
+						activity.getResources(), imageArray.get(position));
+				// Get target image size
+				int bitmapHeight = bitmap.getHeight();
+				int bitmapWidth = bitmap.getWidth();
+
+				BitmapDrawable resizedBitmap = new BitmapDrawable(activity
+						.getResources(), Bitmap.createScaledBitmap(bitmap,
+						width, width * bitmapHeight / bitmapWidth, false));
+
+				// Regular image view
+				// ImageView img = (ImageView)
+				// dialog.findViewById(R.id.imageView1);
+				// img.setBackground(resizedBitmap);
+
+				// TODO set up image view with pinch for zoom - Needs tested on
+				// device not sure it works
+				TouchImageView img = (TouchImageView) dialog
+						.findViewById(R.id.imageView1);
+				img.setBackground(resizedBitmap);
+
 //                Resources res = context.getResources();
 //                try {
 //                    BitmapScaler scaler = new BitmapScaler(res, imageArray.get(position), 100);
@@ -106,22 +109,22 @@ public class SlideshowViewPagerAdapter extends PagerAdapter {
 //                } catch(IOException e) {
 //                    e.printStackTrace();
 //                }
-//
-//				final Button next = (Button) dialog.findViewById(R.id.button1);
-//				next.setBackgroundResource(R.drawable.ic_image_zoom_cross);
-//
-//				next.setOnClickListener(new OnClickListener() {
-//
-//					@Override
-//					public void onClick(View v) {
-//						dialog.dismiss();
-//					}
-//				});
-//				// now that the dialog is set up, it's time to show it
-//				dialog.show();
-//			}
-//
-//		});
+
+				final Button next = (Button) dialog.findViewById(R.id.button1);
+				next.setBackgroundResource(R.drawable.ic_image_zoom_cross);
+
+				next.setOnClickListener(new OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						dialog.dismiss();
+					}
+				});
+				// now that the dialog is set up, it's time to show it
+				dialog.show();
+			}
+
+	});
 		return view;
 	}
 
