f745155831c6cc4b4ba6aea4c8b6f55f01a4751f
mmgoddard <mmgoddard@dundee.ac.uk>
2014-09-30 23:27:20 +0100

    Added error handling messages for syncing.

--- Industrial Project/PotatoIdentifier/src/com/app/potatoidentifer/activities/SearchTest.java
+++ Industrial Project/PotatoIdentifier/src/com/app/potatoidentifer/activities/SearchTest.java
@@ -10,6 +10,8 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import android.net.ConnectivityManager;
+import android.widget.Toast;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -42,10 +44,9 @@ public class SearchTest extends BaseFragment {
     public static final String MyPREFERENCES = "MyPrefs" ;
 	SharedPreferences sharedpreferences;
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Context context = this.getActivity();
+        final Context context = getActivity();
         final View v = inflater.inflate(R.layout.search_test, container, false);
         final GlossaryCategoriesDataSource ds = new GlossaryCategoriesDataSource(context);
         ds.open();
@@ -56,67 +57,89 @@ public class SearchTest extends BaseFragment {
         testBut.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                StrictMode.ThreadPolicy policy = new
-                        StrictMode.ThreadPolicy.Builder().permitAll().build();
-                StrictMode.setThreadPolicy(policy);
 
-                JSONObject json;
-                String str = "";
-                HttpResponse response;
-                HttpClient myClient = new DefaultHttpClient();
-                HttpPost myConnection = new HttpPost("https://zeno.computing.dundee.ac.uk/2014-projects/team1/admin_portal/sync.php");
+                if(checkInternetConnection(context)) {
+                    Toast.makeText(context, "Updating App....", Toast.LENGTH_LONG).show();
+                    StrictMode.ThreadPolicy policy = new
+                            StrictMode.ThreadPolicy.Builder().permitAll().build();
 
-                List nameValuePairs = new ArrayList(1);
+                    StrictMode.setThreadPolicy(policy);
 
-                Date today = Calendar.getInstance().getTime();
-                String currentDate = dateToString(today);
-                String lastUpdated = dateToString(today);
-               
-                nameValuePairs.add(new BasicNameValuePair("currentDate", currentDate));
-                nameValuePairs.add(new BasicNameValuePair("lastUpdated", sharedpreferences.getString("Date","DEFAULT")));
-                Log.v("SHARED","SHARED " + sharedpreferences.getString("Date","DEFAULT"));
-                try {
-                    myConnection.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-                    response = myClient.execute(myConnection);
-                    str = EntityUtils.toString(response.getEntity(), "UTF-8");
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                } catch (ClientProtocolException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                    JSONObject json;
+                    String str = "";
+                    HttpResponse response;
+                    HttpClient myClient = new DefaultHttpClient();
+                    HttpPost myConnection = new HttpPost("https://zeno.computing.dundee.ac.uk/2014-projects/team1/admin_portal/sync.php");
 
-                try {
-                    Log.v("TESTing:", str);
-                    JSONArray jArray = new JSONArray(str);
-                    json = jArray.getJSONObject(0);
-                    for(int i = 0; i < jArray.length(); i++) {
-                        json = jArray.getJSONObject(i);
-                        ds.open();
-                        boolean exists = ds.doesDieaseExistByID(json.getString("_id"));
-                        Log.v("exists", "exists " + exists);
-                        Log.v("ID", "ID: "+json.getString("_id"));
-                        if( exists == true) {
-                            ds.open();
-                            ds.update( json.getString("_id"),json.getString("symptom"), json.getString("type"), json.getString("basicFacts"), json.getString("diagnostics"), json.getString("control"));
-                        }
+                    List nameValuePairs = new ArrayList(1);
+
+                    Date today = Calendar.getInstance().getTime();
+                    String currentDate = dateToString(today);
+                    String lastUpdated = dateToString(today);
+
+                    nameValuePairs.add(new BasicNameValuePair("currentDate", currentDate));
+                    nameValuePairs.add(new BasicNameValuePair("lastUpdated", sharedpreferences.getString("Date", "DEFAULT")));
+                    Log.v("SHARED", "SHARED " + sharedpreferences.getString("Date", "DEFAULT"));
+                    try {
+                        myConnection.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                        response = myClient.execute(myConnection);
+                        str = EntityUtils.toString(response.getEntity(), "UTF-8");
+                    } catch (UnsupportedEncodingException e) {
+                        Toast.makeText(context, "Error with updating the app.", Toast.LENGTH_LONG).show();
+                        e.printStackTrace();
+                    } catch (ClientProtocolException e) {
+                        Toast.makeText(context, "Error with updating the app.", Toast.LENGTH_LONG).show();
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        Toast.makeText(context, "Error with updating the app.", Toast.LENGTH_LONG).show();
+                        e.printStackTrace();
                     }
-                } catch ( JSONException e) {
-                    e.printStackTrace();
+
+                    try {
+                        Log.v("TESTing:", str);
+                        JSONArray jArray = new JSONArray(str);
+                        json = jArray.getJSONObject(0);
+                        for (int i = 0; i < jArray.length(); i++) {
+                            json = jArray.getJSONObject(i);
+                            Log.v("testing", Integer.toString(jArray.length()));
+                            ds.open();
+                            boolean exists = ds.doesDieaseExistByID(json.getString("_id"));
+                            Log.v("exists", "exists " + exists);
+                            Log.v("ID", "ID: " + json.getString("_id"));
+                            if (exists == true) {
+                                ds.open();
+                                ds.update(json.getString("_id"), json.getString("symptom"), json.getString("type"), json.getString("basicFacts"), json.getString("diagnostics"), json.getString("control"));
+                            }
+                        }
+                    } catch (JSONException e) {
+                        Toast.makeText(context, "Error with updating the app.", Toast.LENGTH_LONG).show();
+                        e.printStackTrace();
+                    }
+                    Editor editor = sharedpreferences.edit();
+                    editor.putString("Date", lastUpdated);
+                    editor.commit();
+                    Log.v("SHARED", "SHARED " + sharedpreferences.getString("Date", "DEFAULT"));
+                    Toast.makeText(context, "Update Complete.", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(context, "We can't connect to the Internet right now.", Toast.LENGTH_LONG).show();
                 }
-                Editor editor = sharedpreferences.edit();
-			    editor.putString("Date", lastUpdated);
-			    editor.commit();
-                Log.v("SHARED","SHARED " + sharedpreferences.getString("Date","DEFAULT"));
             }
         });
         return v;
     }
 
-    public String dateToString(Date date) {
+    private String dateToString(Date date) {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String currentDate = formatter.format(date);
         return currentDate;
+    }
+
+    private boolean checkInternetConnection(Context context) {
+        ConnectivityManager conMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (conMgr.getActiveNetworkInfo() != null && conMgr.getActiveNetworkInfo().isAvailable() && conMgr.getActiveNetworkInfo().isConnected()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
