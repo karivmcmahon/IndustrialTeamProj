7afa5b25abcaba2f72f6b8e71eb1cb02b3594ae2
mmgoddard <mmgoddard@dundee.ac.uk>
2014-10-01 14:46:55 +0100

    Fixed some issued with question fragment.

--- Industrial Project/PotatoIdentifier/src/com/app/potatoidentifer/activities/QuestionFragment.java
+++ Industrial Project/PotatoIdentifier/src/com/app/potatoidentifer/activities/QuestionFragment.java
@@ -1,96 +1,57 @@
 package com.app.potatoidentifer.activities;
 
-
-//import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.support.v4.app.FragmentManager;
-import android.graphics.drawable.ColorDrawable;
-
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import com.app.potatoidentifer.models.*;
+import com.app.potatoidentifer.models.FurtherInfoBean;
+import com.app.potatoidentifer.models.QuestionDataSource;
+import com.app.potatoidentifer.models.QuestionEngine;
 import com.example.potatoidentifier.R;
 
 import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 import java.util.List;
-import com.app.potatoidentifer.models.DatabaseHelper;
 
-
-/**
- * This class sets up the fragment for the expert system
- * 
- * @author Kari
- * 
- */
 public class QuestionFragment extends BaseFragment implements View.OnClickListener {
-
-	View v;
+    View v;
     ViewPager myViewPager;
-	TextView textQuestion;
-	Button btnYes, btnNo;
-	
-	QuestionEngine<FurtherInfoBean, String> QE;
-	String currentQuestion;
-	boolean displayingAnswer = false;
-    
-    
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
-
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-		View v = inflater.inflate(R.layout.question_fragment_layout, container, false);
-
-		btnYes = (Button) v.findViewById(R.id.buttonYes);
-		btnNo = (Button) v.findViewById(R.id.buttonNo);
-		textQuestion = (TextView) v.findViewById(R.id.textQuestion);
+    TextView textQuestion;
+    Button btnYes, btnNo;
+    QuestionEngine<FurtherInfoBean, String> QE;
+    String currentQuestion;
+    boolean displayingAnswer = false;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.question_fragment_layout, container, false);
-
-        //savedInstanceState.pu
-
-		btnYes.setOnClickListener(this);
-		btnNo.setOnClickListener(this);
-
-		List<Pair<String, String>> relations = new ArrayList<Pair<String, String>>();
-
-		for (int i = 0; i < QuestionEngine.symptoms.length; i += 2) {
-			relations.add(Pair.create(QuestionEngine.symptoms[i], QuestionEngine.symptoms[i + 1]));
-		}
+        btnYes = (Button) v.findViewById(R.id.buttonYes);
+        btnNo = (Button) v.findViewById(R.id.buttonNo);
+        textQuestion = (TextView) v.findViewById(R.id.textQuestion);
+        btnYes.setOnClickListener(this);
+        btnNo.setOnClickListener(this);
         QuestionDataSource ds = new QuestionDataSource(this.getActivity());
-
         List<Pair<FurtherInfoBean, String>> kno = ds.getKnowledge();
-        
-		QE = new QuestionEngine<FurtherInfoBean, String>(kno);
-
-
-		refresh();
-
-		return v;
-	}
-
-
+        QE = new QuestionEngine<FurtherInfoBean, String>(kno);
+        refresh();
+        return v;
+    }
     protected void refresh()
     {
         if (QE.getPossibleAnswers().size() == 1)
         {
             displayingAnswer = true;
             QE.ForgetAll();
-
-            FurtherInfoBean ans =  QE.getPossibleAnswers().get(0);
+            FurtherInfoBean ans = QE.getPossibleAnswers().get(0);
             Bundle bundle = new Bundle();
             bundle.putString("category", ans.getSymptom());
             FurtherInfo gf = new FurtherInfo();
@@ -103,54 +64,42 @@ public class QuestionFragment extends BaseFragment implements View.OnClickListen
             currentQuestion = QE.determineNextQuestion();
             String q = "Does it have " + currentQuestion + "?";
             textQuestion.setText(q.toCharArray(), 0, q.length());
-
             ArrayList<Bitmap> slideshowImageArray = new ArrayList<Bitmap>();
-
             QE.Inform(currentQuestion, true);
             List<FurtherInfoBean> furtherInfo = QE.getPossibleAnswers();
             QE.Forget(currentQuestion);
-
             for (int i = 0; i < furtherInfo.size(); i++) {
-
                 byte[] id1 = furtherInfo.get(i).getImageID();
                 if (id1 != null && id1.length > 100) {
                     ByteArrayInputStream imageStream = new ByteArrayInputStream(id1);
                     slideshowImageArray.add(BitmapFactory.decodeStream(imageStream));
                 }
-
-               byte[] id2 = furtherInfo.get(i).getImageID2();
+                byte[] id2 = furtherInfo.get(i).getImageID2();
                 if (id2 != null && id2.length > 100) {
                     ByteArrayInputStream imageStream = new ByteArrayInputStream(id2);
                     slideshowImageArray.add(BitmapFactory.decodeStream(imageStream));
                 }
-
                 byte[] id3 = furtherInfo.get(i).getImageID3();
                 if (id3 != null&& id3.length > 100) {
                     ByteArrayInputStream imageStream = new ByteArrayInputStream(id3);
                     slideshowImageArray.add(BitmapFactory.decodeStream(imageStream));
                 }
-
                 byte[] id4 = furtherInfo.get(i).getImageID4();
                 if (id4 != null&& id4.length > 100 ) {
                     ByteArrayInputStream imageStream = new ByteArrayInputStream(id4);
                     slideshowImageArray.add(BitmapFactory.decodeStream(imageStream));
                 }
-
                 byte[] id5 = furtherInfo.get(i).getImageID5();
                 if (id5 != null && id5.length > 100) {
                     ByteArrayInputStream imageStream = new ByteArrayInputStream(id5);
                     slideshowImageArray.add(BitmapFactory.decodeStream(imageStream));
                 }
-
                 byte[] id6 = furtherInfo.get(i).getImageID6();
                 if (id6 != null&& id6.length > 100 ) {
                     ByteArrayInputStream imageStream = new ByteArrayInputStream(id6);
                     slideshowImageArray.add(BitmapFactory.decodeStream(imageStream));
                 }
-
-
             }
-
             SlideshowViewPagerAdapter adapter = new SlideshowViewPagerAdapter(getActivity(), slideshowImageArray, this.getActivity());
             myViewPager = (ViewPager) v.findViewById(R.id.slideshowviewpager);
             myViewPager.setAdapter(adapter);
@@ -159,11 +108,8 @@ public class QuestionFragment extends BaseFragment implements View.OnClickListen
             initButton();
         }
     }
-
-
     private List<Button> buttonArray;
     private int buttonIds[] = {R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4, R.id.btn5, R.id.btn6};
-
     private void initButton() {
         buttonArray = new ArrayList<Button>();
         for (int id : buttonIds) {
@@ -176,7 +122,7 @@ public class QuestionFragment extends BaseFragment implements View.OnClickListen
             buttonArray.add(button);
         }
     }
-
+    
     private void setButton(Button btn, String layoutType) {
         if (layoutType.equals("round_cell_on")) {
             btn.setBackgroundResource(R.drawable.round_cell_on);
@@ -185,17 +131,14 @@ public class QuestionFragment extends BaseFragment implements View.OnClickListen
         }
     }
 
-
     @Override
     public void onClick(View v) {
-
         if (!displayingAnswer) {
             if (btnYes.equals(v)) {
                 QE.Inform(currentQuestion, true);
             } else if (btnNo.equals(v)) {
                 QE.Inform(currentQuestion, false);
             } else {
-
             }
         }
         refresh();
@@ -204,16 +147,13 @@ public class QuestionFragment extends BaseFragment implements View.OnClickListen
     private ViewPager.OnPageChangeListener viewPagerListenerHandler = new ViewPager.OnPageChangeListener() {
         @Override
         public void onPageScrollStateChanged(int position) {}
-
         @Override
         public void onPageScrolled(int arg0, float arg1, int arg2) {}
-
         @Override
         public void onPageSelected(int position) {
             btnAction(position);
         }
     };
-
 
     private void btnAction(int position) {
         Button btn;
@@ -227,5 +167,4 @@ public class QuestionFragment extends BaseFragment implements View.OnClickListen
             }
         }
     }
-
 }
